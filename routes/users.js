3b1559aa2e86c28decac7e96ee45cdf2861395d2
Rose Oyakawa <rose.oyakawa@gmail.com>
2020-05-14 13:25:11 -0300

    instalado o bcrypt, express-validator, method-override

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+const { check, validationResult, body } = require('express-validator');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
