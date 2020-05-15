0a319e4f4b07638462c432a44785449d5c82154b
paulanelo <paula.nelo@fatec.sp.gov.br>
2020-05-15 00:13:26 -0300

    rotas gerais e de usuario

--- routes/index.js
+++ routes/index.js
@@ -1,97 +1,17 @@
 var express = require("express");
 var router = express.Router();
 const { check, validationResult, body } = require("express-validator");
+const IndexController = require('../controllers/IndexController')
+const UserController = require('../controllers/UserController');
 
-const userController = require("../controllers/UserController");
-
-/* GET home page. */
-router.get("/", function (req, res, next) {
-  res.render("index", { title: "Express", logged: false });
-});
-
-router.get("/sobre", (req, res) => {
-  return res.render("screen/about", { logged: false });
-});
-
-router.get("/pets-perdidos-encontrados/novo", (req, res) => {
-  return res.render("screen/register-lost-found-pets", { logged: true });
-});
-
-router.get("/pets-perdidos-encontrados/editar-pet", (req, res) => {
-  return res.render("screen/register-lost-found-pets", { logged: true });
-});
-
-router.get("/user/editar", (req, res) => {
-  return res.render("screen/edit-user", { logged: true });
-});
-
-router.get("/login", userController.showLogin);
-router.post(
-  "/login",
-  [
-    check("email").isEmail().withMessage("Digite um email válido!"),
-    check("senha")
-      .isLength({ min: 6 })
-      .withMessage("A senha tem que conter no mínimo 6 caracteres"),
-  ],
-  userController.logarUsuario
-);
-
-router.get("/esqueci-senha", (req, res) => {
-  return res.render("screen/forgot-password", { logged: false });
-});
-
-router.get("/home/user", (req, res) => {
-  return res.render("screen/home", { logged: true });
-});
-
-router.get("/pets/procurando", (req, res) => {
-  return res.render("screen/lost-found-pets", { logged: true });
-});
-
-router.get("/perfil-pet-adocao", (req, res) => {
-  return res.render("screen/adoption-pets-profile", { logged: true });
-});
-
-router.get("/perfil-pet-encontrado", (req, res) => {
-  return res.render("screen/lost-found-pets-profile", { logged: true });
-});
-
-router.get("/perfil-pet-perdido", (req, res) => {
-  return res.render("screen/lost-found-pets-profile", { logged: true });
-});
-
-// Rota para cadastro de usuário
-router.get("/cadastrar", (req, res) => {
-  return res.render("screen/register-user", { logged: false });
-});
-// rota para gerenciamento de pets cadastrados
-router.get("/user/gerenciamento", (req, res) => {
-  return res.render("screen/manager-pet", {
-    logged: true,
-    nome: "Daia",
-    image: "/images/pet.jpg",
-  });
-});
-// rota para cadastro de pets para adoção
-
-router.get("/user/cadastro-pets-adocao", (req, res) => {
-  return res.render("screen/register-adopted-pets", { logged: true });
-});
-router.get("/perfil-dono", (req, res) => {
-  return res.render("screen/owner-profile", { logged: true });
-});
-// rota para termos de uso
-router.get("/termos-de-uso", (req, res) => {
-  return res.render("screen/terms-of-use", { logged: false });
-});
-// rota para grid de pets perdidos e encontrados
-router.get("/pets-perdidos-encontrados", (req, res) => {
-  return res.render("screen/lost-found-pets", { logged: true });
-});
-// rota para grid de pets perdidos e encontrados
-router.get("/pets-adocao", (req, res) => {
-  return res.render("screen/adoption-pets", { logged: true });
-});
+// add middleware na rota /
+router.get('/', IndexController.auth);
+router.get('/login', IndexController.showLogin);
+router.post('/login', UserController.login);
+router.get('/cadastrar', IndexController.showRegister);
+router.post('/cadastrar', UserController.store);
+// router.get('/recuperar-senha', IndexController.showRecover);
+router.get('/sobre', IndexController.showAbout);
+router.get('/termos-de-uso', IndexController.showTerms);
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+const UserController = require('../controllers/UserController')
+
+router.get('/user', UserController.getUser); // busca o usuario com base do id que está no cookie e mostra a página de usuario com as informações 
+
+router.get('/user/:id', UserController.show); // busca o usuario com base no id da rota e exibe a pagina de usuario com as informações
+
+router.get('/user/editar', UserController.showUpdate); // mostra o form
+router.update('/user/editar', UserController.update); // action do form
+
+router.get('/user/gerenciamento', UserController.showGerenciamento); // mostra a tela de gerenciamento
 
 module.exports = router;
