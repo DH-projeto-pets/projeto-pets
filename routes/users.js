0f7f8c7b06d3760df4ba8184c4d10277309e6065
paulanelo <paula.nelo@fatec.sp.gov.br>
2020-05-15 00:15:20 -0300

    corrigindo erro de método

--- routes/users.js
+++ routes/users.js
@@ -8,7 +8,7 @@ router.get('/user', UserController.getUser); // busca o usuario com base do id q
 router.get('/user/:id', UserController.show); // busca o usuario com base no id da rota e exibe a pagina de usuario com as informações
 
 router.get('/user/editar', UserController.showUpdate); // mostra o form
-router.update('/user/editar', UserController.update); // action do form
+router.put('/user/editar', UserController.update); // action do form
 
 router.get('/user/gerenciamento', UserController.showGerenciamento); // mostra a tela de gerenciamento
 
