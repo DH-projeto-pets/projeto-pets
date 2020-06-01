ff2ffdac7ce8d009ef74b9d2ead286417ecaa57b
paulanelo <paula.nelo@fatec.sp.gov.br>
2020-06-01 00:03:41 -0300

    img upload controller

--- controllers/PetController.js
+++ controllers/PetController.js
@@ -1,93 +1,115 @@
-const { sequelize, Pet } = require("../models");
+const { sequelize, Pet, Foto } = require("../models");
 const { Op } = require("sequelize");
 module.exports = {
   showGrid: async (req, res) => {
     const pets = await Pet.findAll({
       where: {
-        [Op.or]: [
-          { status: 'PERDIDO' },
-          { status: 'ENCONTRADO' }
-        ]
-      }
+        [Op.or]: [{ status: "PERDIDO" }, { status: "ENCONTRADO" }],
+      },
     });
-    res.render('screen/lost-found-pets', { pets })
+    res.render("screen/lost-found-pets", { pets });
   },
   showGridAdocao: async (req, res) => {
     const pets = await Pet.findAll({
       where: {
         status: {
-          [Op.eq]: 'ADOCAO'
+          [Op.eq]: "ADOCAO",
         },
-      }
+      },
     });
-    res.render('screen/adoption-pets', { pets })
+    res.render("screen/adoption-pets", { pets });
   },
   showPetPerfil: async (req, res) => {
     const { id } = req.params;
     const pet = await Pet.findOne({
       where: {
-        id
+        id,
       },
-      include: ["raca"]
+      include: ["raca", "fotos"],
     });
-    res.render('screen/lost-found-pets-profile', { pet })
+    res.render("screen/lost-found-pets-profile", { pet });
   },
-  showPetCadastro: (req, res) => res.render('screen/register-lost-found-pets'),
+  showPetCadastro: (req, res) => res.render("screen/register-lost-found-pets"),
   showPetEdicao: async (req, res) => {
     const pet = await Pet.findOne({
       where: {
-        [Op.or]: [{ status: 'ENCONTRADO' }, { status: 'PERDIDO' }],
-        [Op.and]: [
-          { id: req.params.id },
-          { fk_usuario: req.session.user.id }
-        ]
-      }
-
-    })
-    console.log(pet)
-    res.render('screen/edit-lost-found-pets', { pet })
+        [Op.or]: [{ status: "ENCONTRADO" }, { status: "PERDIDO" }],
+        [Op.and]: [{ id: req.params.id }, { fk_usuario: req.session.user.id }],
+      },
+    });
+    console.log(pet);
+    res.render("screen/edit-lost-found-pets", { pet });
   },
 
-  showPetCadastroAdocao: (req, res) => res.render('screen/register-adopted-pets'),
+  showPetCadastroAdocao: (req, res) =>
+    res.render("screen/register-adopted-pets"),
   showPetEdicaoAdocao: async (req, res) => {
     const pet = await Pet.findOne({
       where: {
         [Op.and]: [
           { fk_usuario: req.session.user.id },
           { id: req.params.id },
-          { status: 'ADOCAO' }
-        ]
-      }
-    })
-    console.log(pet)
-    res.render('screen/edit-adopted-pets', { pet })
+          { status: "ADOCAO" },
+        ],
+      },
+    });
+    console.log(pet);
+    res.render("screen/edit-adopted-pets", { pet });
   }, // Rose
-
 
   // controla o banco
 
-
   update: async (req, res) => {
-
-    const pet = await Pet.update({
-      ...req.body
-    },
-      { where: { id: req.params.id } },
-
+    const pet = await Pet.update(
+      {
+        ...req.body,
+      },
+      { where: { id: req.params.id } }
     );
     return res.redirect("/user/gerenciamento");
-
   },
 
   store: async (req, res) => {
-    console.log(req.body);
+    // console.log("Files ", req);
 
     const pet = await Pet.create({
       ...req.body,
       fk_usuario: req.session.user.id,
-      fk_raca: req.body.raca
-    }).then(pet => pet).catch(err => err);
-    console.log("==>", pet)
+      fk_raca: req.body.raca,
+    })
+      .then((pet) => pet)
+      .catch((err) => err);
+
+    if (pet) {
+      const images = req.files.map((file) => ({
+        caminho: `/images/${file.originalname}-${Math.floor(
+          Math.random() * 1000
+        )}`,
+        fk_pet: pet.id,
+      }));
+
+      // await Foto.bulkCreate(images);
+
+      for (img of images) {
+        await Foto.create({
+          ...img,
+        });
+      }
+
+      const [caminho] = images;
+      const foto = await Foto.findOne({
+        where: {
+          caminho,
+        },
+      });
+
+      await Pet.update(
+        {
+          fk_foto_principal: foto.id,
+        },
+        { where: { id: pet.id } }
+      );
+    }
 
     res.redirect("/user/gerenciamento");
   },
@@ -96,15 +118,10 @@ module.exports = {
     const { id: userId } = req.session.user;
     const pet = await Pet.destroy({
       where: {
-        [Op.and]: [
-          { id: petId },
-          { fk_usuario: userId }
-        ]
-      }
+        [Op.and]: [{ id: petId }, { fk_usuario: userId }],
+      },
     });
 
     res.redirect("/user/gerenciamento");
   },
-
 };
-
