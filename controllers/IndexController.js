f50ec083672c11e8535eb2f33678089930672e00
paulanelo <paula.nelo@fatec.sp.gov.br>
2020-06-02 17:21:16 -0300

    login validator complete v2

--- controllers/IndexController.js
+++ controllers/IndexController.js
@@ -1,20 +1,21 @@
-const { sequelize, Pet } = require('../models');
+const { sequelize, Pet } = require("../models");
 
 module.exports = {
   auth: async (req, res) => {
     // se estiiver logado vai pra home.ejs e se nao vai index
-    const pets = await Pet.findAll()
+    const pets = await Pet.findAll();
 
-    return res.render('screen/home', { pets })
+    return res.render("screen/home", { pets });
   },
   logout: (req, res) => {
     req.session.destroy(() => {
       return res.redirect("/");
     });
   },
-  showLogin: (req, res) => res.render('screen/login'),
-  showRegister: (req, res) => res.render('screen/register-user'),
+  showLogin: (req, res) =>
+    res.render("screen/login", { errors: {}, usuario: {} }),
+  showRegister: (req, res) => res.render("screen/register-user"),
   //showRecover: (req, res) => res.render(),
-  showAbout: (req, res) => res.render('screen/about'),
-  showTerms: (req, res) => res.render('screen/terms-of-use'),
+  showAbout: (req, res) => res.render("screen/about"),
+  showTerms: (req, res) => res.render("screen/terms-of-use"),
 };
