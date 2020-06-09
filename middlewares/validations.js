1fa2c4436b99b73e136c79338d3bec2510f9c708
marcoaureliomenezes <marcoaurelioreislima@gmail.com>
2020-06-09 18:19:21 -0300

    Mudanças nos formularios de edição de pets (adoção e perdidos ou encontrados, e nos controllers para realizar validation.

--- middlewares/validations.js
+++ middlewares/validations.js
@@ -1,19 +1,38 @@
 const { check,
-    validationResult,
-    body
+  validationResult,
+  body
 } = require('express-validator');
 
-const petAdoptionValidation = [ check("status").isIn(['ADOCAO']),
-check("porte")
-  .isIn(['GRANDE', 'MEDIO','PEQUENO'])
-  .withMessage("Campo obrigatório"),
-check('especie').notEmpty().withMessage("Campo obrigatório"),
-check('raca').notEmpty().withMessage("Campo obrigatório"),
-check("sexo").isIn(['FEMEA','MACHO','DESCONHECIDO']).withMessage("Campo obrigatório"),
-check("castrado").isIn(['0','1']).withMessage("Campo obrigatório."),
-check("vermifugado").isIn(['0','1']).withMessage("Campo obrigatório"),
-check("vacinado").isIn(['0','1']).withMessage("Campo obrigatório")];
+const statusValidation = 
+[check("status").isIn(['PERDIDO', 'ENCONTRADO']).withMessage("Campo obrigatório")]
+const petValidation = [ check("status").isIn(['ADOCAO']),
+  check("porte")
+    .isIn(['GRANDE', 'MEDIO','PEQUENO'])
+    .withMessage("Campo obrigatório"),
+  check('especie')
+    .notEmpty()
+    .withMessage("Campo obrigatório"),
+  check('raca')
+    .notEmpty()
+    .withMessage("Campo obrigatório"),
+  check("sexo")
+    .isIn(['FEMEA','MACHO','DESCONHECIDO'])
+    .withMessage("Campo obrigatório"),
+];
+
+const petAdoptionValidation = [ 
+  check("castrado")
+    .isIn(['0','1'])
+    .withMessage("Campo obrigatório."),
+  check("vermifugado")
+    .isIn(['0','1'])
+    .withMessage("Campo obrigatório"),
+  check("vacinado")
+    .isIn(['0','1'])
+    .withMessage("Campo obrigatório")
+  ];
 
 
-module.exports = { petAdoptionValidation }
+module.exports = {statusValidation, petValidation, petAdoptionValidation }
+
 
--- routes/pets.js
+++ routes/pets.js
@@ -1,7 +1,8 @@
 const { Router } = require("express");
 const { check, validationResult, body } = require("express-validator");
 const router = Router();
-const { petAdoptionValidation } = require('../middlewares/validations')
+const { petAdoptionValidation, statusValidation,
+  petValidation } = require('../middlewares/validations')
 const PetController = require("../controllers/PetController");
 
 const { checkUser } = require("../middlewares");
@@ -10,20 +11,18 @@ const upload = require("../helpers/upload");
 router.get("/", PetController.showGrid);
 router.get("/adocao", PetController.showGridAdocao);
 router.get("/cadastrar", checkUser, PetController.showPetCadastro);
-router.post(
-  "/cadastrar",
-  // [check("status").isEmpty(), check("raca").isEmpty(), check("especie").isEmpty(), check("porte").isEmpty(), check("sexo").isEmpty(),],
-  [check("status").isIn(['oi', 'tchau']).withMessage('Este campo deve ser preenchido!')],
-  upload.array("fotos"),
-  PetController.store
-);
+router.post("/cadastrar", statusValidation, petValidation,
+upload.array("fotos"), PetController.store);
 router.get("/adocao/cadastrar", checkUser, PetController.showPetCadastroAdocao);
-router.post("/adocao/cadastrar", upload.array("fotos"), petAdoptionValidation, PetController.store);
+router.post("/adocao/cadastrar", upload.array("fotos"),
+ petValidation, petAdoptionValidation, PetController.store);
 
 router.get("/:id/editar", checkUser, PetController.showPetEdicao);
-router.put("/:id/editar", PetController.update);
+router.put("/:id/editar", upload.array("fotos"),
+statusValidation, petValidation, PetController.update);
 router.get("/adocao/:id/editar", checkUser, PetController.showPetEdicaoAdocao);
-router.put("/adocao/:id/editar", PetController.update);
+router.put("/adocao/:id/editar", upload.array("fotos"),
+    petValidation, petAdoptionValidation, PetController.update);
 router.get("/:id", PetController.showPetPerfil);
 router.delete("/delete", PetController.delete);
 
