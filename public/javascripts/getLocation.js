f32cd8150d1b0c863278f52eb4193bc6cf2a75a6
paulanelo <paula.nelo@fatec.sp.gov.br>
2020-06-11 14:54:27 -0300

    geo location

--- public/javascripts/getLocation.js
+++ public/javascripts/getLocation.js
@@ -1,24 +1,27 @@
 const btnLocation = document.querySelector('[data-button="getAddress"]');
 if (btnLocation) {
-  btnLocation.addEventListener('click', () => {
+  btnLocation.addEventListener("click", () => {
     navigator.geolocation.getCurrentPosition(success, error, options);
-  })
+  });
 }
 
 var options = {
   enableHighAccuracy: true,
   timeout: 5000,
-  maximumAge: 0
+  maximumAge: 0,
 };
 
-function success(pos) {
-  var { latitude, longitude } = pos.coords;
-  const address = fetch(`https://maps.googleapis.com/maps/api/geocode/json?latlng=${latitude},${longitude}&key=AIzaSyDiPqRxxOBUUX9-H8OUnjgZQZukxX98TjE`).then(res => res.json())
+async function success(pos) {
+  var { latitude, longitude, accuracy, altitudeAccuracy } = pos.coords;
+  console.log(accuracy);
+  const address = await fetch(
+    `https://maps.googleapis.com/maps/api/geocode/json?latlng=${latitude},${longitude}&key=AIzaSyDiPqRxxOBUUX9-H8OUnjgZQZukxX98TjE`
+  ).then((res) => res.json());
   console.log(address);
   // console.log('Longitude: ' + crd.longitude);
-  console.log('Mais ou menos ' + crd.accuracy + ' metros.');
-};
+  // console.log('Mais ou menos ' + crd.accuracy + ' metros.');
+}
 
 function error(err) {
-  console.warn('ERROR(' + err.code + '): ' + err.message);
-};
+  console.warn("ERROR(" + err.code + "): " + err.message);
+}
